678e48482c69ecafa7e4aad93f8b80d8261ad176
sixpagers <sixpagers@gmail.com>
2020-07-18 18:01:28 -0500

    Dashboard changes

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -251,7 +251,7 @@ toggleProBanner() {
         <div className="row proBanner">
           <div className="col-12">
             <span className="d-flex align-items-center purchase-popup">
-              <p>Like what you see? Check out our premium version for more.</p>
+              <p>Welcome to Alpha Montessori</p>
               <a href="https://github.com/BootstrapDash/StarAdmin-Free-React-Admin-Template" target="_blank" rel="noopener noreferrer" className="btn ml-auto download-button">Download Free Version</a>
               <a href="http://www.bootstrapdash.com/demo/star-admin-pro-react/template/demo_1/preview/dashboard" rel="noopener noreferrer" target="_blank" className="btn purchase-button">Upgrade To Pro</a>
               <i className="mdi mdi-close bannerClose" onClick={this.toggleProBanner}></i>
